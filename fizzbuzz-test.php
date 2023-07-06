ec6f450233716bf26838c7ea7a4d3a37292e44f1
Tri Wahyono <triwahyono1024@gmail.com>
2023-07-06 08:43:14 +0700

    tes

---
+++ fizzbuzz-test.php
@@ -0,0 +1,19 @@
+<?php
+echo "No 1 \n 
+Buat loop yang mencetak angka 1-100, JIKA kelipatan 3 maka outputnya Fizz , JIKA kelipatan 5 outputnya Buzz dan JIKA kelipatan 3 dan 5 keluarkan FizzBuzz
+\n";
+
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        echo "FizzBuzz\n";
+    } elseif ($i % 3 == 0) {
+        echo "Fizz\n";
+    } elseif ($i % 5 == 0) {
+        echo "Buzz\n";
+    } else {
+        echo $i . "\n";
+    }
+}
+echo "================================================ \n";
+
+?>
---
+++ palindrome-test.php
@@ -0,0 +1,34 @@
+<?php
+
+$input1 = "SAIPPUAKIVIKAUPPIAS";
+$input2 = "Aibohphobia";
+$input3 = "Anna";
+$input4 = "Civic";
+$input5 = "My gym";
+$input6 = "No lemon, no melon";
+
+$input6 = strtolower(str_replace(' ', '', $input6));
+
+$array = str_split($input6);
+
+
+for ($i=0;$i<count($array);$i++){
+    
+    for($j= count($array)-$i-1;$j>=0;$j--){
+        if($array[$i]==$array[$j]){
+            $palindrome="ya";
+        }else{
+            $palindrome="tidak";
+            break;
+        }
+        break;
+    }
+}
+
+if($palindrome=="ya")
+echo "true";
+if($palindrome=="tidak")
+echo "false";
+
+
+?>
